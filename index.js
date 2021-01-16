1d7bd54fad60facc25670dd6dccd63a917e36707
blaadje <acharlot91@gmail.com>
2021-01-16 14:45:40 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
+  console.log(req)
   res.send('Hello World!')
 })
 
